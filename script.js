313472508cc75a49f15601ff0f175019ae7225d6
Sam-1 <saver0aro@gmail.com>
2024-12-02 01:12:10 +0100

    Updated splash

--- script.js
+++ script.js
@@ -2,12 +2,15 @@ document.addEventListener("DOMContentLoaded", () => {
   const splash = document.querySelector(".splash");
   const splashImage = document.querySelector("#splash");
 
-  splashImage.src = "images/lahaine.gif?" + new Date().getTime();
+  splashImage.src = "images/lahaine.gif?v=" + new Date().getTime();
 
   splashImage.onload = () => {
+    const gifDuration = splashImage.naturalDuration || 5610;
 
-    setTimeout(() => {
-      splash.classList.add("display-none");
-    }, splashImage.naturalDuration || 5610);
+    window.requestAnimationFrame(() => {
+      setTimeout(() => {
+        splash.classList.add("display-none");
+      }, gifDuration);
+    });
   };
 });
